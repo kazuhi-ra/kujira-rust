44146d86239be049922ea79f850c73d6c0b532da
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-09 19:17:19 +0900

    for inに肩を付ける

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    for i500 in 0..11 {
-        for i100 in 0..4 {
-            for i50 in 0..11 {
+    for i500 in 0u16..11 {
+        for i100 in 0u16..4 {
+            for i50 in 0u16..11 {
                 let amount = 500 * i500 + 100 * i100 + 50 * i50;
                 if amount == 3950 {
                     println!("500円: {}枚, 100円: {}枚, 50円: {}枚", i500, i100, i50);
