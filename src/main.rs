60ebff18f1c6824f3850b31e0a2d4086f0a4b361
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-09 15:50:15 +0900

    九九の表を作る

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 fn main() {
-    for i in 1..101 {
-        if i % 15 == 0 {
-            println!("FizzBuzz");
-        } else if i % 3 == 0 {
-            println!("Fizz");
-        } else if i % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}", i);
+    for a in 1..10 {
+        for b in 1..10 {
+            print!("{:3}", a * b);
+            if b != 9 {
+                print!(",");
+            }
         }
+        println!("");
     }
 }
