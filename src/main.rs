b3f80cf8c929e2ed59e00b1fffbf3e276d087042
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-10 15:44:14 +0900

    attempt to multiply with overflow

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,4 @@
-fn is_prime(n: usize) -> bool {
-    for i in 2..n {
-        if n % i == 0 {
-            return false;
-        }
-    }
-    return true;
-}
-
-fn get_primses(primes: &mut [usize; 100]) -> () {
-    let mut i = 2;
-    let mut count = 0;
-
-    while count < 100 {
-        if is_prime(i) {
-            primes[count] = i;
-            count += 1;
-        }
-        i += 1;
-    }
-}
-
 fn main() {
-    let mut primes = [0; 100];
-    get_primses(&mut primes);
-    println!("{:?}", primes);
-
-    let mut v: u32 = 10;
-    set_value(&mut v);
-    print!("{}", v);
-}
-
-fn set_value(arg: &mut u32) {
-    *arg = 4649;
+    let v: u128 = 1234;
+    println!("{}", v.pow(5678))
 }
