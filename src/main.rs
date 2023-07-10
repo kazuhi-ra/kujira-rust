360eb70c78b19b0e74dcae262a44ac81ab337710
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-11 01:23:14 +0900

    ベクター型とビンゴカードの作成

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,23 @@
-use rand::Rng;
-
-const MAP_N: usize = 25;
+use rand::seq::SliceRandom;
 
 fn main() {
-    let mut maze = [[false; MAP_N]; MAP_N];
-
-    // 外壁
-    for n in 0..MAP_N {
-        let row = &mut maze[n];
-        if n == 0 || n == MAP_N - 1 {
-            row.fill(true);
-        } else {
-            row[0] = true;
-            row[MAP_N - 1] = true;
-        }
+    let mut nums: Vec<u8> = vec![];
+    for i in 1..=75 {
+        nums.push(i)
     }
 
-    for n in 2..(MAP_N - 2) {
-        if n % 2 == 0 {
-            for m in 2..(MAP_N - 2) {
-                if m % 2 == 0 {
-                    // マス配置
-                    maze[n][m] = true;
-                    // 倒す
-                    match randam() {
-                        0 => maze[n - 1][m] = true,
-                        1 => maze[n][m + 1] = true,
-                        2 => maze[n + 1][m] = true,
-                        3 => maze[n][m - 1] = true,
-                        _ => {}
-                    }
-                }
-            }
-        }
-    }
-
-    // print
-    for n in 0..MAP_N {
-        let row = maze[n];
-        for e in row {
-            let result = if e { '❌' } else { '　' };
-            print!("{}", result);
-        }
-        println!("");
-    }
-}
-
-fn randam() -> u8 {
     let mut rng = rand::thread_rng();
-    return rng.gen_range(0..=3);
+    nums.shuffle(&mut rng);
+
+    for i in 0..25 {
+        let x = if i == 12 {
+            "*".to_string()
+        } else {
+            nums[i].to_string()
+        };
+        print!("{:3}", x);
+        if i % 5 == 4 {
+            println!("")
+        }
+    }
 }
