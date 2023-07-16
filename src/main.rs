5527bd6c2012cc22200f173c2600b6d0d18df364
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-16 14:42:59 +0900

    配列・vectorの比較ができることを確認する

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ fn main() {}
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4)
+    fn array() {
+        let a1 = [100, 200, 300];
+        let a2 = [100, 200, 300];
+        assert_eq!(a1, a2)
+    }
+
+    #[test]
+    fn vec() {
+        let v1: Vec<u16> = vec![100, 200, 300];
+        let v2: Vec<u16> = vec![100, 200, 300];
+        assert_eq!(v1, v2)
     }
 }
