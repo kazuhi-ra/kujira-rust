defad7ea50a8e98468412b04b815bfc43a240fb2
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-16 15:33:32 +0900

    構造体にメソッドを定義する

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct User {
     name: String,
     age: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::User;
-    #[test]
-    fn strc() {
-        let u1 = User {
-            name: "kazuhira".to_string(),
-            age: 27,
-        };
-        let u2 = User {
-            name: "kirito".to_string(),
-            age: 16,
-        };
-
-        assert_eq!(u1, u2)
+impl User {
+    fn give_ones_name(&self) {
+        println!("I am {}.", self.name)
     }
+}
+
+fn main() {
+    let kirito = User {
+        name: String::from("kirito"),
+        age: 16,
+    };
+
+    kirito.give_ones_name()
 }
