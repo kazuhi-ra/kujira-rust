ab36af21aa70c23e023bebadff906b1f1e08e0d4
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-16 22:46:47 +0900

    モジュールを使ってみる

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,19 @@
-enum Coin {
-    Coin1(isize),
-    Coin5(isize),
-    Coin10(isize),
-    Coin50(isize),
-    Coin100(isize),
-    Coin500(isize),
-}
+mod util {
+    pub fn fizz_buzz() {
+        for i in 1..=50 {
+            let result = match (i % 3, i % 5) {
+                (0, 0) => "FizzBuzz".to_string(),
+                (0, _) => "Fizz".to_string(),
+                (_, 0) => "Buzz".to_string(),
+                _ => format!("{}", i),
+            };
 
-impl Coin {
-    fn calc_price(&self) -> isize {
-        match *self {
-            Coin::Coin1(v) => 1 * v,
-            Coin::Coin5(v) => 5 * v,
-            Coin::Coin10(v) => 10 * v,
-            Coin::Coin50(v) => 50 * v,
-            Coin::Coin100(v) => 100 * v,
-            Coin::Coin500(v) => 500 * v,
+            println!("{}", result)
         }
     }
 }
 
+use util::fizz_buzz;
 fn main() {
-    let wallet: [Coin; 6] = [
-        Coin::Coin1(3),
-        Coin::Coin5(10),
-        Coin::Coin10(5),
-        Coin::Coin50(1),
-        Coin::Coin100(1),
-        Coin::Coin500(5),
-    ];
-
-    let total = wallet
-        .iter()
-        .fold(0, |sum, current| sum + current.calc_price());
-
-    println!("{}", total)
+    fizz_buzz()
 }
