c8130c0f2c10686ea92d214cd8bd3c0401949002
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-16 14:37:15 +0900

    テストを書いてみる

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-fn main() {
-    let a = "kazuhi-ra".to_string();
+fn main() {}
 
-    print_string(&a);
-    println!("{}", a);
-}
-
-fn print_string(s: &String) -> () {
-    println!("{}", s);
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4)
+    }
 }
