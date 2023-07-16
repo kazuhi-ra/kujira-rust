215d82a0364a82d88327fe10f60d41615c8419af
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-16 20:45:41 +0900

    構造体のメソッド定義でジェネリクスを指定

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-fn main() {
-    let a = add(-10, 42_i64);
-    println!("{}", a);
-
-    let b = add(10_u8, 42);
-    println!("{}", b);
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
-fn add<T>(a: T, b: T) -> T
+impl<T> Point<T>
 where
-    T: std::ops::Add<Output = T>,
+    T: std::ops::AddAssign,
 {
-    a + b
+    fn new(x: T, y: T) -> Self {
+        Point { x, y }
+    }
+
+    fn add(&mut self, pt: Self) {
+        self.x += pt.x;
+        self.y += pt.y;
+    }
+}
+
+fn main() {
+    let mut p1 = Point::new(3, 8);
+    let p2 = Point::new(-2, 4);
+
+    p1.add(p2);
+
+    println!("{:?}", p1)
 }
