6642627e7f048983b7ff087cc2dcd3d0a7d75c08
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-16 23:01:58 +0900

    ファイル分割してみる

---
+++ src/utils.rs
@@ -0,0 +1,12 @@
+pub fn fizz_buzz() {
+    for i in 1..=50 {
+        let result = match (i % 3, i % 5) {
+            (0, 0) => "FizzBuzz".to_string(),
+            (0, _) => "Fizz".to_string(),
+            (_, 0) => "Buzz".to_string(),
+            _ => format!("{}", i),
+        };
+
+        println!("{}", result)
+    }
+}
