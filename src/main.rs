3de00ea85229c2ba66be41f9f6140137c7af64a9
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-16 22:12:32 +0900

    enumを使ってみる

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,38 @@
-struct FibIterator {
-    a: usize,
-    b: usize,
+enum Coin {
+    Coin1(isize),
+    Coin5(isize),
+    Coin10(isize),
+    Coin50(isize),
+    Coin100(isize),
+    Coin500(isize),
 }
 
-impl FibIterator {
-    fn new() -> Self {
-        FibIterator { a: 1, b: 1 }
-    }
-}
-
-impl Iterator for FibIterator {
-    type Item = usize;
-    fn next(&mut self) -> Option<Self::Item> {
-        let tmp = self.a;
-        self.a = self.b;
-        self.b = self.a + tmp;
-        Some(self.a)
+impl Coin {
+    fn calc_price(&self) -> isize {
+        match *self {
+            Coin::Coin1(v) => 1 * v,
+            Coin::Coin5(v) => 5 * v,
+            Coin::Coin10(v) => 10 * v,
+            Coin::Coin50(v) => 50 * v,
+            Coin::Coin100(v) => 100 * v,
+            Coin::Coin500(v) => 500 * v,
+        }
     }
 }
 
 fn main() {
-    let fib_iter = FibIterator::new();
+    let wallet: [Coin; 6] = [
+        Coin::Coin1(3),
+        Coin::Coin5(10),
+        Coin::Coin10(5),
+        Coin::Coin50(1),
+        Coin::Coin100(1),
+        Coin::Coin500(5),
+    ];
 
-    for (i, u) in fib_iter.enumerate() {
-        if i >= 10 {
-            break;
-        }
-        println!("{}", u)
-    }
+    let total = wallet
+        .iter()
+        .fold(0, |sum, current| sum + current.calc_price());
+
+    println!("{}", total)
 }
