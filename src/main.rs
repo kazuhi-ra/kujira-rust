516fa89b3e6822cc7671b59c1b8a5714c985ac21
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-17 18:07:39 +0900

    120x120の市松模様のpngを生成する

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,21 @@ fn main() {
     let white = image::Rgb::<u8>([255, 255, 255]);
     let red = image::Rgb::<u8>([255, 90, 90]);
 
-    // let w = 64;
-    let draw = |x, y| match (x % 2, y % 2) {
-        (0, 0) => white,
-        (1, 0) => red,
-        (0, 1) => red,
-        (1, 1) => white,
-        (_, _) => panic!("error"),
+    let length = 120;
+    let draw = |x, y| {
+        let block_length = length / 10;
+        let (xi, yi) = (x / block_length, y / block_length);
+
+        return match (xi % 2, yi % 2) {
+            (0, 0) => white,
+            (1, 0) => red,
+            (0, 1) => red,
+            (1, 1) => white,
+            (_, _) => panic!("error"),
+        };
     };
 
-    let img = image::ImageBuffer::from_fn(10, 10, draw);
+    let img = image::ImageBuffer::from_fn(length, length, draw);
 
-    match img.save("sample.png") {
-        Ok(_) => println!("ok"),
-        Err(e) => println!("{}", e),
-    }
+    img.save("sample.png").unwrap();
 }
