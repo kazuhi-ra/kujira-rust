4debe754d7d27c7b40d34393f6d5212ffc2a9a18
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-17 23:06:52 +0900

    macroを自作してみる

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-fn main() {
-    let white = image::Rgb::<u8>([255, 255, 255]);
-    let red = image::Rgb::<u8>([255, 90, 90]);
-
-    let length = 120;
-    let draw = |x, y| {
-        let block_length = length / 10;
-        let (xi, yi) = (x / block_length, y / block_length);
-
-        return match (xi % 2, yi % 2) {
-            (0, 0) => white,
-            (1, 0) => red,
-            (0, 1) => red,
-            (1, 1) => white,
-            (_, _) => panic!("error"),
-        };
+macro_rules! echo_num {
+    ($num:expr) => {
+        println!("{}", $num);
     };
+}
 
-    let img = image::ImageBuffer::from_fn(length, length, draw);
-
-    img.save("sample.png").unwrap();
+fn main() {
+    echo_num!(1);
+    echo_num![2];
+    echo_num! {3};
 }
